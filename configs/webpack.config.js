e4f4b1dfe0ca3a8bd97abd286c8181d04b299c6b
Egor Blinov <egor.b@miro.com>
2021-06-22 19:21:05 +0300

    add react

--- configs/webpack.config.js
+++ configs/webpack.config.js
@@ -1,9 +1,22 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
-const resolveApp = (pathToResolve) => path.resolve(__dirname, '..', 'src', pathToResolve);
+const resolveApp = (pathToResolve) =>
+    path.resolve(__dirname, '..', 'src', pathToResolve);
 
 module.exports = {
-    entry: { index: resolveApp('index.js') },
+    entry: { index: resolveApp('index.jsx') },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
     plugins: [new HtmlWebpackPlugin()],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+        ],
+    },
 };
---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return 'Hello world'
+}
+
+ReactDOM.render(<App/>, document.querySelector('body'));
